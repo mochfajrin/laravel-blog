cf9d380e3cdb7a38e81c434a75d7b6481ffb567c
homurin <mochfajrin1211@gmail.com>
2024-12-30 21:25:21 +0700

    feat: blog page

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,10 +5,15 @@ namespace App\Models;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Post extends Model
 {
     use HasFactory;
+    public function author()
+    {
+        return $this->belongsTo(User::class, "user_id");
+    }
     public function scopePublished($query)
     {
         $query->where("published_at", "<=", Carbon::now());
@@ -16,5 +21,13 @@ class Post extends Model
     public function scopeFeatured($query)
     {
         $query->where("featured", true);
+    }
+    public function getExcerpt()
+    {
+        return Str::limit(strip_tags($this->body), 150);
+    }
+    public function getReadingTime()
+    {
+        return ceil(str_word_count($this->body) / 250);
     }
 }
