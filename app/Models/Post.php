779430989aa798bef0d6d288ce62c3d2394b781d
homurin <mochfajrin1211@gmail.com>
2025-01-01 17:30:46 +0700

    feat: fillament admin panel

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,14 +5,29 @@ namespace App\Models;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Str;
 
 class Post extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+    protected $fillable = [
+        "user_id",
+        "title",
+        "slug",
+        "body",
+        "image",
+        "published_at",
+        "featured"
+    ];
     public function author()
     {
         return $this->belongsTo(User::class, "user_id");
+    }
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
     }
     public function scopePublished($query)
     {
