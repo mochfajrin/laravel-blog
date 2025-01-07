39e868e4f836f89b6a3cdbd155fc87d5f4c91e79
homurin <mochfajrin1211@gmail.com>
2025-01-07 17:40:05 +0700

    feat: caching

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,21 +3,28 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class HomeController extends Controller
 {
     public function __invoke(Request $request)
     {
-        $featuredPosts = Post::published()
-            ->with("categories")
-            ->featured()
-            ->latest("published_at")
-            ->take(3)->get();
-        $latestPosts = Post::published()
-            ->with("categories")
-            ->latest("published_at")
-            ->take(9)->get();
+        $featuredPosts =
+            Cache::remember("featuredPosts", Carbon::now()->addDay(), function () {
+                return Post::published()
+                    ->with("categories")
+                    ->featured()
+                    ->latest("published_at")
+                    ->take(3)->get();
+            });
+        $latestPosts = Cache::remember("latestPost", Carbon::now()->addDay(), function () {
+            return Post::published()
+                ->with("categories")
+                ->latest("published_at")
+                ->take(9)->get();
+        });
         return view("home", [
             "featuredPost" => $featuredPosts,
             "latestPost" => $latestPosts,
