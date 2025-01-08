69f836d612e80da194305b7e36c07bb777dccfd1
homurin <mochfajrin1211@gmail.com>
2025-01-08 12:31:35 +0700

    feat: language switcher

---
+++ app/Http/Middleware/LanguageMiddleware.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class LanguageMiddleware
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (session()->has('locale') && array_key_exists(session('locale'), config('app.supported_locales'))) {
+            app()->setLocale(session('locale'));
+        }
+
+        return $next($request);
+    }
+}
--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,5 +1,14 @@
-<footer class="text-sm space-x-4 flex items-center border-t border-gray-100 flex-wrap justify-center py-4 ">
-    <a class="text-gray-500 hover:text-red-700" href="{{ route('home') }}">{{ __('menu.home') }}</a>
-    <a class="text-gray-500 hover:text-red-700" href="{{ route('profile.show') }}">{{ __('menu.profile') }}</a>
-    <a class="text-gray-500 hover:text-red-700" href="{{ route('posts.index') }}">{{ __('menu.blog') }}</a>
+<footer class="text-sm px-6 flex items-center border-t border-gray-100 flex-wrap justify-between py-4 ">
+    <div class="space-x-4">
+        <a class="text-gray-500 hover:text-red-700" href="{{ route('home') }}">{{ __('menu.home') }}</a>
+        <a class="text-gray-500 hover:text-red-700" href="{{ route('profile.show') }}">{{ __('menu.profile') }}</a>
+        <a class="text-gray-500 hover:text-red-700" href="{{ route('posts.index') }}">{{ __('menu.blog') }}</a>
+    </div>
+    <div class="flex gap-2">
+        @foreach (config('app.supported_locales') as $locale => $data)
+            <a href="{{ route('language', ['locale' => $locale]) }}">
+                <x-dynamic-component :component="'flag-country-' . $data['icon']" class="w6 h-6" />
+            </a>
+        @endforeach
+    </div>
 </footer>
